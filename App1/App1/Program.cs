05a08914118c4ddfabe58b873a92ae7d22f8bdb8
Sanskar Ravula <sanskarravula13@gmail.com>
2024-01-19 14:35:59 +0530

    test 4

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -24,6 +24,7 @@ namespace App1
             Console.WriteLine("Test 1");
             Console.WriteLine("Test 2");
             Console.WriteLine("Test 3");
+            Console.WriteLine("Test 4");
         }
     }
 }
